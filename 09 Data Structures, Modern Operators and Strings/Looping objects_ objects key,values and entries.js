69019361cf72e5ba7092858d6b76e2affe6bcde0
kaifabbas110 <121484498+kaifabbas110@users.noreply.github.com>
2023-02-18 15:39:46 +0500

    Add files via upload

---
+++ 09 Data Structures, Modern Operators and Strings/Looping objects_ objects key,values and entries.js
@@ -0,0 +1,54 @@
+const restaurant = {
+    name: 'Kaifi Da Cafe',
+    location: 'Korangi No 4, Karachi',
+    categories: ['Italian', 'Vegetarian', 'Organic', 'Meat'],
+    starterMenu: ['Cheese', 'Bread', 'Salad', 'Raw Meat'],
+    mainMenu: ['Burger', 'Spring Roll', 'Pizza'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22
+        },
+        fri: {
+            open: 11,
+            close: 23
+        },
+        sat: {
+            open: 0,
+            close: 24
+        }
+    },
+};
+
+//Return as array so than we can easily loop over objects.
+
+//Keys of Object
+//Keys are object of object
+console.log(Object.keys(restaurant.openingHours));
+for (const days of Object.keys(restaurant.openingHours)) {
+    console.log(days);
+}
+
+
+//Values of Object
+//Values are Object's inside key values.
+//Object.values stores objects in an array.
+console.log(Object.values(restaurant.openingHours));
+for (const timing of Object.values(restaurant.openingHours)) {
+    console.log(timing);
+}
+//Here de-structuring object to get opening & closing timings.
+for (const {open,close} of Object.values(restaurant.openingHours)) {
+    console.log(open,close);
+}
+
+//Entries Object
+//It store object keys and index[0] & 
+console.log(Object.entries(restaurant.openingHours));
+for (const days of Object.entries(restaurant.openingHours)) {
+    console.log(days);
+}
+//Here de-structuring object to get days also opening & closing timings.
+for (const [day,{close,open}] of Object.entries(restaurant.openingHours)) {
+    console.log(`On ${day} we open at ${open}${(open >= 12)?'pm':'am'} & close at ${close}${(close >= 12)?'pm':'am'}`);
+}
